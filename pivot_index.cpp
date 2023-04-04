98aa06e38569cd6238032483ab30a44519e33c9e
Ishan Grover <123376573+IshanGrover2004@users.noreply.github.com>
2023-04-04 12:50:00 +0530

    Add files via upload

---
+++ pivot_index.cpp
@@ -0,0 +1,10 @@
+// Created by grove on 01-04-2023.
+// 724. Find Pivot Index Leetcode question
+/* The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the sum of all the numbers strictly to the index's right.
+if array is {1,7,3,6,5,6} then pivot_index = 3 bcs 1+7+3=5+6 */
+
+#include <iostream>
+using namespace std;
+int main(){
+
+}
